ec237d71e54b69d2460162312e0b4fa0e8555412
W0439218 <W0439218@172LAB-DDXGNF3>
2025-09-17 12:42:37 -0300

    Add project files.

---
+++ SuperTix/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SuperTix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
