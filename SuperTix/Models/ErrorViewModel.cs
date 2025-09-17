c3ee685283d575d76bf57b1bdb139dcf8e432a36
howellAj <W0439218@campus.nscc.ca>
2025-09-17 13:47:36 -0300

    Boilerplate removed, Data Model created

--- SuperTix/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SuperTix.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ SuperTix/Models/Ticket.cs
@@ -0,0 +1,31 @@
+﻿namespace SuperTix.Models
+{
+    public class Ticket
+    {
+        //PK
+        public int TicketID { get; set; }
+        //FK
+        public int EventID { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public string EventName { get; set; }
+
+        public string TicketHolderName { get; set; }
+
+        public string Address { get; set; }
+     
+        public decimal Price { get; set; }
+
+        public string SeatNumber { get; set; }
+
+        public Boolean IsAvailable { get; set; }
+
+
+
+
+
+
+
+    }
+}
