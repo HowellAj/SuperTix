8b94686b2c92140df5ee6559e24322ebc877a340
HowellAj <W0439218@campus.nscc.ca>
2025-09-26 19:06:09 -0300

    Before Any migrations

---
+++ SuperTix/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperTix.Models
+
+{
+    public class Category
+    {
+        //PK
+        public int CategoryId { get; set; } 
+
+        public string? CategoryName { get; set; }
+
+        public List<Game>? Games { get; set; }
+    }
+}
---
+++ SuperTix/Models/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace SuperTix.Models
+{
+    public class Game
+    {
+        //PK
+        public int GameId { get; set; }
+
+        //FK to Category
+        public int CategoryId { get; set; }
+
+        public string? GameName { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime GameDate { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string? Owner { get; set; }
+
+        public string? Location { get; set; }
+
+        public Category? Category { get; set; }
+
+    }
+}
